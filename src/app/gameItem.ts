be653b59d1028b5594f39e7987132811b368a5fc
GabrielePanarello <gbrlpanarello@gmail.com>
2018-02-14 09:27:39 +0100

    Setup GameItem

---
+++ src/app/gameItem.ts
@@ -0,0 +1,19 @@
+export class GameItem{
+    id: string;
+    nome: string;
+    descrizione: string;
+    genere: string;
+    rating: number;
+    prezzo: number;
+    annoUscita: number;
+
+    constructor(id:string = "", nome:string = "", descrizione:string = "", genere:string= "", rating:number=0, prezzo:number=0, annoUscita:number = 0){
+        this.id = id;
+        this.nome = nome;
+        this.genere = genere;
+        this.rating = rating;
+        this.prezzo = prezzo;
+        this.annoUscita = annoUscita;
+    }
+
+}
