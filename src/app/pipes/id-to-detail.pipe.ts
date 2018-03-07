bba000bbf293617f5ddaa7d6c13ebe451855d682
GabrielePanarello <gbrlpanarello@gmail.com>
2018-03-07 22:19:18 +0100

    Update Pipe

--- src/app/pipes/id-to-detail.pipe.ts
+++ src/app/pipes/id-to-detail.pipe.ts
@@ -1,14 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { GenreService } from '../services/genre.service';
+import { GameListService } from '../services/game-list-service';
 
 @Pipe({
   name: 'idToDetail'
 })
 export class IdToDetailPipe implements PipeTransform {
 
-  transform(id: string): string {
-    let genresService: GenreService = new GenreService();
-    return genresService.getDescById(id);
+  constructor(private gameListService: GameListService){}
+
+  transform(id: string, itemId: string): string {
+    return this.gameListService.getGameById(itemId).genere.descrizione;
   }
 
 }
